a6aa6ff239f1f214fb77f39ce8bdd5045e4f3c23
Lamprini Papargyri <80075276+papargyri@users.noreply.github.com>
2021-07-16 23:52:34 +0300

    Update password.py

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
+# A password, sometimes called a passcode, is a memorized secret, typically a string of characters, usually used to confirm the identity of a user.
+
+# Random module is used to perform the random generations. 
+
 import string
 import random
 
 def gen():
-    s1 = string.ascii_uppercase
-    s2 = string.ascii_lowercase
+    s1 = string.ascii_uppercase  # All Upper case letters
+    s2 = string.ascii_lowercase  # All Lower case letters
     s3 = string.digits
-    s4 = string.punctuation
+    s4 = string.punctuation  # String of ASCII characters which are considered punctuation characters in the C locale.
 
     passlength = int(input("Enter the password length\n"))
 
