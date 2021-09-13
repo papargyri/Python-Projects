e3527bdc03cf8e655f96a4303cdd1c826c101f96
Lamprini Papargyri <80075276+papargyri@users.noreply.github.com>
2021-09-14 00:15:25 +0300

    Update Celsius2Fahrenheit.py

--- Celsius2Fahrenheit.py
+++ Celsius2Fahrenheit.py
@@ -1,5 +1,5 @@
-temp = float(input("Please enter the temperature in Celsius: "))
+celsius = float(input("Please enter the temperature in Celsius: "))
 
-fahrenheit = (temp * 1.8) + 32  
+fahrenheit = (celsius * 1.8) + 32  
 
-print(f"{temp} in Celsius is equal to {round(fahrenheit,2)} in Fahrenheit")
+print('%0.1f degrees in Celsius is equal to %0.1f degrees in Fahrenheit'% (celsius,fahrenheit))
